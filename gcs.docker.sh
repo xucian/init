47bd0c58df53291a5ed2c2fdb498d9177664c044
xucian <5688727+xucian@users.noreply.github.com>
2023-07-05 13:59:01 +0300

    Update gcs.docker.sh

--- gcs.docker.sh
+++ gcs.docker.sh
@@ -21,35 +21,31 @@ set +e
 sudo groupadd docker || echo "Skipping 'sudo groupadd docker' as it already exists"
 set -e
 sudo usermod -aG docker $USER
-# Trying to reload group. Might not work actually
-exec newgrp docker
+echo "Trying to run docker login in a separate process as it's not possible to reload the group and continue executing the current script"
 
-
-# Login to docker
-echo "Checking if curl is installed"
-(which curl &> /dev/null && echo "Curl is installed") || \
-    (echo "Installing curl" && \
-    apt-get update -y -qq > /dev/null && \
-    apt-get install -y -qq curl > /dev/null)
-
+	
 # Thanks https://medium.com/@sachin.d.shinde/docker-compose-in-container-optimized-os-159b12e3d117
 token=$(curl --fail --silent --show-error --header  "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token")
 token=$(echo "$token" | grep --extended-regexp --only-matching "(ya29.[0-9a-zA-Z._-]*)")
 
-docker_login_cmd () 
-{
-    local repo=${1}
+sudo runuser -l $USER <<EOF
+	# Login to docker
+	echo "Checking if curl is installed"
+	(which curl &> /dev/null && echo "Curl is installed") || \
+	    (echo "Installing curl" && \
+	    apt-get update -y -qq > /dev/null && \
+	    apt-get install -y -qq curl > /dev/null)
+	
+	
+	# Fixes " error storing credentials - err: exit status 1, out: add is unimplemented"
+	# As per https://github.com/docker/cli/issues/1136#issuecomment-491610567
+	docker logout
+	mv ~/.docker/config.json ~/.docker/config_old.json
+	
+	echo "$token" | docker login -u oauth2accesstoken --password-stdin europe-docker.pkg.dev > /dev/null
+	echo "$token" | docker login -u oauth2accesstoken --password-stdin us-docker.pkg.dev > /dev/null
+	echo "$token" | docker login -u oauth2accesstoken --password-stdin asia-docker.pkg.dev > /dev/null
 
-    echo "$token" | docker login -u oauth2accesstoken --password-stdin "$repo"
-}
-
-# Fixes " error storing credentials - err: exit status 1, out: `add is unimplemented"
-# As per https://github.com/docker/cli/issues/1136#issuecomment-491610567
-docker logout
-mv ~/.docker/config.json ~/.docker/config_old.json
-
-docker_login_cmd europe-docker.pkg.dev > /dev/null
-docker_login_cmd us-docker.pkg.dev > /dev/null
-docker_login_cmd asia-docker.pkg.dev > /dev/null
-
+EOF
+	
 echo "Done docker login"
